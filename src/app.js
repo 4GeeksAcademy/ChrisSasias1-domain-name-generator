26856ba61ad233d93873771e1cb8c7acd45e3401
Christopher Sasias <christophersasias@hotmail.com>
2025-10-29 14:06:28 +0000

    Domain Generate Complete

--- src/app.js
+++ src/app.js
@@ -1,11 +1,44 @@
 import "bootstrap";
 import "./style.css";
 
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+let extension = ['.com', '.uy', '.net', '.br'];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let result = [];
+
+function nameTheDomain() {
+
+  for (let c = 0; c < pronoun.length; c++) {
+    for (let h = 0; h < adj.length; h++) {
+      for (let r = 0; r < noun.length; r++) {
+        for (let i = 0; i < extension.length; i++) {
+
+         result.push(pronoun[c] + adj[h] + noun[r] + extension[i])
+
+        }
+      }
+    }
+  }
+
+return result;
+}
+
+function renderDomains() {
+  const list = document.getElementById("domain-list");
+  const domains = nameTheDomain();
+
+
+  list.innerHTML = "";
+
+
+  for (let i = 0; i < domains.length; i++) {
+    const li = document.createElement("li");
+    li.textContent = domains[i];
+    list.appendChild(li);
+  }
+}
+
+renderDomains();
